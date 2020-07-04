3b9074d0d31875fbe8de65b805cc4882349646d7
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-07-04 16:39:26 -0400

    Added feature of being able to work with a single record

--- frontend/components/Course.js
+++ frontend/components/Course.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import { Text } from '@airtable/blocks/ui';
+import React, { useState } from 'react';
+import { Text, Button, useRecordActionData, useBase, useRecordById} from '@airtable/blocks/ui';
+// import { selectRecords } from '@airtable/blocks';
+
 const Course = () => {
+    const [assigned, setAssigned] = useState(false);
+    const base = useBase();
+    const data = useRecordActionData();
+    const tableId = 'tblNoIGLVD8wIXWKG';
+    const recordId = 'recjXyRPgRX7gKAu8';
+    const table = base.getTableById(data ? data.tableId : tableId);
+    const record = useRecordById(table, data ? data.recordId : recordId);
+
+    console.log('record', record);
     const course = {
-        name: "The course that Jeff takes",
+        name: record.getCellValue('Name'),
         dateTime: new Date(),
         instructor: "Jeff",
         room: {
@@ -10,15 +21,21 @@ const Course = () => {
             capacity: 40,
         }
     }
-    return <div>
-        <Text>Course Name: {course.name}</Text>
-        <Text>Date and Time: {course.dateTime.toDateString()}</Text>
-        <Text>Instructor Name: {course.instructor}</Text>
-        <div>
-            <Text>Room Details</Text>
-            <Text>Selected Room: {course.selectedRoom}</Text>
-            <Text>No. of seats: {toString(course.capacity)}</Text>
-        </div>
-    </div>;
-}
+    const check = <Button>Assigned</Button>
+        const courseDisplay = <div>
+            <Text>Course Name: {course.name}</Text>
+            <Text>Date and Time: {course.dateTime.toDateString()}</Text>
+            <Text>Instructor Name: {course.instructor}</Text>
+            <div>
+                <Text>Room Details</Text>
+                <Text>Selected Room: {course.selectedRoom}</Text>
+                <Text>No. of seats: {toString(course.capacity)}</Text>
+            </div>
+            <Button
+                onClick={() => setAssigned(!assigned)}
+                icon="multicollaborator">Assign Rooms</Button>
+            {assigned ? check : null}
+        </div>;
+    return courseDisplay;
+    }
 export default Course;
--- frontend/components/Temp.js
+++ frontend/components/Temp.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { useBase, useRecords, Record } from '@airtable/blocks/ui';
+import { useBase, useRecords} from '@airtable/blocks/ui';
 // renders a list of tables and automatically updates
 const Temp = () => {
     const base = useBase();
     const table = base.tables[0];
     const records = useRecords(table, {fields: ['Name']});
-    console.log('lasoon');
     return (
         <ul>
             {records.map(record => {
